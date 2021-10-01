a5da540feb38f47638b553f2a79efcddb270a680
Rakibul Hasan <rifat20089@hotmail.com>
2021-10-01 12:46:21 +0600

    file setup done

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  // eslint-disable-next-line react-native/no-inline-styles
+  return <Text style={{textAlign: 'center', marginTop: 50}}>Hello World</Text>;
+};
+
+export default App;
