cd2566a76161b516b213c1a32281bf79e30db8d0
Rakibul Hasan <rifat20089@hotmail.com>
2021-10-06 12:48:30 +0600

    swipe animation fully working

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   Text,
   Image,
@@ -20,32 +20,39 @@ import Animated, {
   event,
   useDerivedValue,
   interpolate,
+  runOnJS,
 } from 'react-native-reanimated';
 import {PanGestureHandler} from 'react-native-gesture-handler';
 
 // object for declired details for user
 const ROTATION = 40;
+const SWIPE_VELOCITY = 800;
 
 // Main app function
 const App = () => {
-  // state profile of users
+  // STATE OF USER
   const [currentIndex, setCurrentIndex] = useState(0);
-  const currentProfile = users[currentIndex];
+  const [nextIndex, setNextIndex] = useState(currentIndex + 1);
 
-  // screenWidth
+  const currentProfile = users[currentIndex];
+  const nextProfile = users[nextIndex];
+
+  // SCREEN WIDTH
   const {width: screenWidth} = useWindowDimensions();
 
   // OUT OF THE SCREEN
   const hiddenTranslateX = 2 * screenWidth;
 
-  // Animation
+  // ANIMATION
+  // value calculation for the rotate and translate
   const translateX = useSharedValue(0); //-screenWidth    0     screenWidth
   const rotate = useDerivedValue(
     () =>
       interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) +
       'deg',
-  ); //-60deg          0deg  60deg
+  ); //-60deg          0deg       60deg
 
+  // rotate and transform animation of the cards
   const cardStyle = useAnimatedStyle(() => ({
     transform: [
       {
@@ -57,7 +64,25 @@ const App = () => {
     ],
   }));
 
-  // Gesture Handler
+  // next card comming up and opacity animation
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: interpolate(
+          translateX.value,
+          [-hiddenTranslateX, 0, hiddenTranslateX],
+          [1, 0.9, 1],
+        ),
+      },
+    ],
+    opacity: interpolate(
+      translateX.value,
+      [-hiddenTranslateX, 0, hiddenTranslateX],
+      [1, 0.4, 1],
+    ),
+  }));
+
+  // GUSTURE HENDLER
   const gestureHendler = useAnimatedGestureHandler({
     onStart: (_, context) => {
       context.startX = translateX.value;
@@ -65,13 +90,37 @@ const App = () => {
     onActive: (event, context) => {
       translateX.value = context.startX + event.translationX;
     },
-    onEnd: () => {},
+    onEnd: event => {
+      // bring the card bacl in the position
+      if (Math.abs(event.velocityX) < SWIPE_VELOCITY) {
+        translateX.value = withSpring(0);
+
+        return;
+      }
+
+      // shipw the card out of the screen
+      translateX.value = withSpring(
+        hiddenTranslateX * Math.sign(event.velocityX),
+        {},
+        () => runOnJS(setCurrentIndex)(currentIndex + 1), // next card being current card
+      );
+    },
   });
 
-  // Mobile View
+  // set next index
+  useEffect(() => {
+    translateX.value = 0;
+    setNextIndex(currentIndex + 1);
+  }, [currentIndex, translateX]);
+
+  // VIEW
   return (
     <View style={styles.pageContainer}>
-      
+      <View style={styles.nextCardContainer}>
+        <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+          <Card user={nextProfile} />
+        </Animated.View>
+      </View>
 
       <PanGestureHandler onGestureEvent={gestureHendler}>
         <Animated.View style={[styles.animatedCard, cardStyle]}>
@@ -88,6 +137,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+
   },
   animatedCard: {
     width: '100%',
@@ -99,6 +149,7 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
     alignItems: 'center',
+    // zIndex: -1,
   },
 });
 
